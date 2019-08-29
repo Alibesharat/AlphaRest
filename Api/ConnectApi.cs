ede7a13f5d30d0c7dd74dd182fbdfbd9fba6e51c
Ali Besharati <1368.ali.b@gmail.com>
2019-08-29 15:37:30 +0430

    init

---
+++ Api/ConnectApi.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AlphaRest.Api
+{
+
+
+    public  static partial class ConnectApi
+    {
+        static HttpClient client = new HttpClient();
+
+
+        /// <summary>
+        /// Send File From HttpClinet
+        /// </summary>
+        /// <param name="file">IFormFile file</param>
+        /// <param name="AllowdExtention">set allowed AllowdExtention to Post file set with dot like .png</param>
+        /// <param name="GatewayUrl">api url</param>
+        /// <param name="maxLength">max Length in Byte</param>
+        /// <returns>dynamic Type and ResultConteract </returns>
+        public static async Task<dynamic> SendFileAsync(IFormFile file, List<string> AllowdExtentions, int maxLength, string GatewayUrl)
+        {
+            try
+            {
+
+                if (file == null && file.Length <= 0)
+                    return ("File Not Found");
+                var ext = file.FileName.Substring(file.FileName.LastIndexOf('.')).ToLower();
+                if (!AllowdExtentions.Contains(ext))
+                    return ($"Extention : ({ext}) is Not Allowd !");
+                if (file.Length > maxLength)
+                    return ($"file  Length: ({file.Length}) is bigger than {maxLength} !");
+
+                try
+                {
+
+                    byte[] data;
+                    using (var br = new BinaryReader(file.OpenReadStream()))
+                        data = br.ReadBytes((int)file.OpenReadStream().Length);
+
+                    ByteArrayContent bytes = new ByteArrayContent(data);
+                    MultipartFormDataContent multiContent = new MultipartFormDataContent
+                        {
+                            { bytes, "file",file.FileName }
+                        };
+                    var result = await client.PostAsync(GatewayUrl, multiContent);
+                    result.EnsureSuccessStatusCode();
+                    var resultConteract = await result.Content.ReadAsStringAsync();
+                    if (!string.IsNullOrWhiteSpace(resultConteract))
+                    {
+
+                        return resultConteract;
+
+                    }
+                    else
+                    {
+                        return ("Get Result was faild");
+
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    return (ex);
+
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return (ex);
+            }
+
+
+        }
+
+
+        /// <summary>
+        /// Send Model From HttpClinet
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <param name="Url"></param>
+        /// <param name="methode"> def = Post </param>
+        /// <returns></returns>
+        public static async Task<T> SendModel<T>(object value, string Url, ApiMethode methode = ApiMethode.Post)
+        {
+            T Data = default(T);
+            try
+            {
+                ServicePointManager.Expect100Continue = false;
+                switch (methode)
+                {
+                    case ApiMethode.Post:
+
+                        var result = await client.PostAsJsonAsync(Url, value);
+                        result.EnsureSuccessStatusCode();
+                        Data = await result.Content.ReadAsAsync<T>();
+                        break;
+                    case ApiMethode.Get:
+                        var results = await client.GetAsync(Url);
+                        results.EnsureSuccessStatusCode();
+                        Data = await results.Content.ReadAsAsync<T>();
+                        // Data = JsonConvert.DeserializeObject<T>(results);
+
+                        break;
+                }
+                return Data;
+            }
+            catch (System.Exception ex)
+            {
+
+                //TODO : Log Ex
+                return Data;
+            }
+        }
+
+
+    }
+    public enum ApiMethode
+    {
+
+        Post,
+        Get
+    }
+
+
+
+
+
+}
+
+
---
+++ file/File.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace AlphaRest.file
+{
+    public static class File
+    {
+        /// <summary>
+        /// save File with auto uniq Name in Specific Path 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="UploadPath">UploadPath can be like this : WebRootPath/Folder/Subfolder/SubOfSubFolder</param>
+        /// <param name="AllowdExtention">WhitListTosave : new List<string>(){png,gif}</param>
+        /// <returns>Return a tuple of bool an string  to chack state and name of file</returns>
+        public static async Task<(bool, string)> SaveFileAsync(IFormFile file, string UploadPath, List<string> AllowdExtention)
+        {
+
+            try
+            {
+                if (file != null)
+                {
+                    string Message = Message = "فایل وارد شده مجاز نمی باشد";
+
+                    //check file Type in Dot net core is Null 
+                    //file.ContentType.StartsWith(fileType.ToString()))
+                    string Extention = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+                    if (!AllowdExtention.Contains(Extention))
+                    {
+                        return (false, Message);
+                    }
+                    if (!Directory.Exists(UploadPath))
+                    {
+                        Directory.CreateDirectory(UploadPath);
+                    }
+                    string uinqName = Generatetoken();
+                    var fileName = $"{uinqName}.{Extention}";
+
+                    var filePath = Path.Combine(UploadPath, fileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream).ConfigureAwait(false);
+                    }
+                    return (true, fileName);
+                }
+                else
+                {
+                    return (false, "No Files Found");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+
+
+        /// <summary>
+        /// save File with auto uniq Name in Specific Path 
+        /// </summary>
+        /// <param name="file"></param>
+        /// <param name="Name">the Name without Extention</param>
+        /// <param name="UploadPath">UploadPath can be like this : WebRootPath/Folder/Subfolder/SubOfSubFolder</param>
+        /// <param name="AllowdExtention">WhitListTosave : new List<string>(){png,gif}</param>
+        /// <returns>Return a tuple of bool an string  to chack state and name of file</returns>
+        public static async Task<(bool, string)> SaveFileAsync(IFormFile file,string Name, string UploadPath, List<string> AllowdExtention)
+        {
+
+            try
+            {
+                if (file != null)
+                {
+                    string Message = Message = "فایل وارد شده مجاز نمی باشد";
+
+                    //check file Type in Dot net core is Null 
+                    //file.ContentType.StartsWith(fileType.ToString()))
+                    string Extention = file.FileName.Substring(file.FileName.LastIndexOf('.'));
+                    if (!AllowdExtention.Contains(Extention))
+                    {
+                        return (false, Message);
+                    }
+                    if (!Directory.Exists(UploadPath))
+                    {
+                        Directory.CreateDirectory(UploadPath);
+                    }
+                   
+                    var fileName = $"{Name}.{Extention}";
+
+                    var filePath = Path.Combine(UploadPath, fileName);
+                    using (var fileStream = new FileStream(filePath, FileMode.Create))
+                    {
+                        await file.CopyToAsync(fileStream).ConfigureAwait(false);
+                    }
+                    return (true, fileName);
+                }
+                else
+                {
+                    return (false, "No Files Found");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                return (false, ex.Message);
+            }
+        }
+
+
+        public static string Generatetoken()
+        {
+            return Regex.Replace(Convert.ToBase64String(Guid.NewGuid().ToByteArray()), "[/+=]", "");
+        }
+
+    }
+}
